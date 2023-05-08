bcd7387e96b5d51dee910dbaccd50a09c3ab8d7a
Sujoy Giri <girisujoy630@gmail.com>
2023-05-09 00:18:42 +0530

    added auth service and auth guard

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {Subscription} from 'rxjs'
+
+import { GlobalService } from 'src/app/global.service';
+import { LoginData } from 'src/app/model/auth-data-model';
 
 @Component({
   selector: 'app-signin',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent {
+  loginSubscription!:Subscription;
+  constructor(private globalService:GlobalService) {}
+  login(data:LoginData){
+    this.loginSubscription = this.globalService.login(data).subscribe({
+      next: (result:any) => {
+        console.log(result);
+      },
+      error: (error:any) => {
+        console.log(error);
+      },
+      complete: () => {
+        console.log("complete");
+      }
+    })
+
+  }
 
 }
--- src/app/global.service.ts
+++ src/app/global.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 
+import { LoginData } from './model/auth-data-model';
+
 @Injectable({
   providedIn: 'root'
 })
 export class GlobalService {
 
-  url:string = "http://localhost:3000/api/v1/"
+  url:string = "http://localhost:3000/api/auth/login"
 
   constructor(private http:HttpClient) { }
 
-  login(){
-    return this.http.post()
+  login(data:LoginData){
+    return this.http.post(this.url,{email:data.email,password:data.password},{withCredentials:true})
   }
 }
